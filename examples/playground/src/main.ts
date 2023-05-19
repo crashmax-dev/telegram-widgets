83559db3b282f43c1b188066376c89e8a0ad3832
Vitalij Ryndin <crashmax.dev@gmail.com>
2023-05-19 21:25:09 +0800

    chore(examples): update playground

--- examples/playground/src/main.ts
+++ examples/playground/src/main.ts
@@ -1,16 +1,42 @@
-import { TelegramAuthService } from '@telegram-widgets/core'
+import formatHighlight from '@crashmax/json-format-highlight'
+import { AuthService } from '@telegram-widgets/core'
+import { el } from '@zero-dependency/dom'
+import './style.css'
 
-const botId = '2107099955'
-const origin = 'telegram-widgets.com'
+const root = document.querySelector('#root')!
 
-const authService = new TelegramAuthService({
-  botId,
-  origin
+const loginButton = el(
+  'button',
+  {
+    onclick(event) {
+      event.preventDefault()
+      authService.auth()
+    }
+  },
+  'Login with Telegram'
+)
+
+const loginData = el('code')
+
+const authService = new AuthService({
+  botId: '2107099955',
+  origin: 'telegram-widgets.com',
+  language: 'en',
+  requestAccess: false,
+  autorizationType: 'redirect',
+  redirectUrl: 'https://telegram-widgets.com/api/login',
+  onError(error) {
+    console.error(error)
+  },
+  onAuth(user) {
+    loginData.innerHTML = formatHighlight(user, {
+      tagPre: true,
+      wordWrap: false,
+      colors: {
+        falseColor: '#f44747'
+      }
+    })
+  }
 })
 
-const authButton = document.querySelector('#auth-button')!
-authButton.addEventListener('click', () => {
-  authService.auth((user) => {
-    console.log(user)
-  })
-})
+root.append(loginButton, loginData)
