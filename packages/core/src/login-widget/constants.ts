892ec9c0f526bcc7637c0a9d270de0e4792d53c0
Vitalij Ryndin <crashmax.dev@gmail.com>
2023-05-25 21:42:26 +0800

    remove core files

--- packages/core/src/login-widget/constants.ts
+++
@@ -1,2 +0,0 @@
-export const WIDGET_AUTH_URL = 'https://oauth.telegram.org'
-export const SECONDS_TO_EXPIRE = 86400
--- packages/core/src/login-widget/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './auth-widget.js'
-export * from './auth-validate.js'
-export * from './types.js'
